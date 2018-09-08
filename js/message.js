dbff89246c21a11e8f9265d4d43569e6f8772ca7
xiaoxinwan <lixinxi1010@gmail.com>
2018-09-08 14:16:23 +0800

    add model

--- js/message.js
+++ js/message.js
@@ -1,57 +1,80 @@
-//init
-var APP_ID = 'xkEaHgwQriprwGMQ16trRlwn-gzGzoHsz';
-var APP_KEY = 'WXFE0MM9fM8g5cWrSQWSiTAr';
+! function() {
+    var view = document.querySelector('section.message')
 
+    var model = {
+        init: function() {
+            var APP_ID = 'xkEaHgwQriprwGMQ16trRlwn-gzGzoHsz';
+            var APP_KEY = 'WXFE0MM9fM8g5cWrSQWSiTAr';
+            AV.init({
+                appId: APP_ID,
+                appKey: APP_KEY
+            })
+        },
+        fetch: function() {
+            var query = new AV.Query('Message');
+            return query.find()
+        },
+        save: function(name,content) {
+            var Message = AV.Object.extend('Message')
+            var message = new Message()
+            return message.save({
+                'name': name,
+                'content': content
+            })
+        }
 
-AV.init({
-    appId: APP_ID,
-    appKey: APP_KEY
-});
-
-var query = new AV.Query('Message');
-query.find()
-	.then(function(messages) {
-    	let array = messages.map((item) => item.attributes)
-    	console.log('array')
-    	console.log(array)
-    	array.forEach ((item) => {
-    		let li = document.createElement('li')
-    		li.innerText = item.contents
-    		console.log(item)
-    		console.log(li.innerText)
-    		
-    		let messageList = document.querySelector('#messageList')
-    		messageList.appendChild(li)
-    	})
     }
-    ,function(error) {
-    // 异常处理
-}).then(() => {},(error) => {
-	console.log(error)
-});
 
-//存入数据
-let myForm = document.querySelector('#postMessageForm')
+    var controller = {
+        view: null,
+        model: null,
+        messageList: null,
+        init: function(view, model) {
+            this.view = view
+            this.model = model
 
-myForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    let content = myForm.querySelector('input[name=content]').value
-    var Message = AV.Object.extend('Message')
-    var message = new Message()
-    message.save({
-        'contents': content
-    }).then(function(object) {
-        window.location.reload()
-        console.log(object)
-    })
-})
+            this.messageList = view.querySelector('#messageList')
+            this.form = view.querySelector('#postMessageForm')
+            this.model.init()
+            this.loadMessages()
+            this.bindEvents()
+        },
+        loadMessages: function() {
+            this.model.fetch()
+                .then((messages) => {
+                    let array = messages.map((item) => item.attributes)
+                    array.forEach((item) => {
+                        let li = document.createElement('li')
+                        li.innerText = `${item.name}: ${item.content}`
+                        this.messageList.appendChild(li)
+                    })
+                });
+        },
+        bindEvents: function() {
+            this.form.addEventListener('submit', (e) => {
+                e.preventDefault()
+                this.saveMessage()
+            })
+        },
+        saveMessage: function() {
+            let myForm = this.form
+
+            let content = myForm.querySelector('input[name=content]').value
+            let name = myForm.querySelector('input[name=name]').value
+            this.model.save(name,content)
+                .then(function(object) {
+                    let li = document.createElement('li')
+                    li.innerText = `${object.attributes.name}: ${object.attributes.content}`
+                    let messageList = document.querySelector('#messageList')
+                    messageList.appendChild(li)
+                    myForm.querySelector('input[name=content]').value = ''
+                    myForm.querySelector('input[name=name]').value = ''
+                    console.log(object)
+                })
+        }
+
+    }
+    controller.init(view, model)
 
 
-//test
-// var TestObject = AV.Object.extend('TestObject');
-// var testObject = new TestObject();
-// testObject.save({
-//   words: 'lxx来啦！！！！！！！！！！！'
-// }).then(function(object) {
-//   console.log('1234567');
-// })
+}.call()
